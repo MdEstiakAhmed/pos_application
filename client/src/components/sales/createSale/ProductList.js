703053af8e8a9dc9ef8dc8970c1d25d9f8a0ba3d
MdEstiakAhmed <estiak97@gmail.com>
2020-12-17 18:20:41 +0600

    product search in cart

--- client/src/components/sales/createSale/ProductList.js
+++ client/src/components/sales/createSale/ProductList.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from 'react';
 const ProductList = (props) => {
     const {cart, setCart} = props;
     const [product, setProduct] = useState([]);
+    const [searchProduct, setSearchProduct] = useState([])
 
     useEffect(() => {
         fetch('http://localhost:5000/product', {
@@ -17,9 +18,11 @@ const ProductList = (props) => {
         .then(data => {
             if(data.status){
                 setProduct(data.data);
+                setSearchProduct(data.data)
             }
             else{
                 setProduct([]);
+                setSearchProduct([])
             }
         })
         .catch(error => {
@@ -28,6 +31,8 @@ const ProductList = (props) => {
     }, [])
 
     const handleCart = (obj) => {
+        document.getElementById('searchProduct').value = '';
+        setSearchProduct(product);
         if(obj.quantity > 0){
             obj.count = 1;
             if(cart.length === 0){
@@ -42,6 +47,20 @@ const ProductList = (props) => {
         }
     }
 
+    const handleProductList = (str) => {
+        if(str !== ""){
+            let temp = product.filter(item => {
+                if(item.name.includes(str) || item.barcode.includes(str)){
+                    return item;
+                }
+            })
+            setSearchProduct(temp);
+        }
+        else{
+            setSearchProduct(product);
+        }
+    }
+
     return (
         <>
             <div className="dropdown mb-5">
@@ -49,10 +68,14 @@ const ProductList = (props) => {
                     Dropdown button
                 </button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <div className="p-2">
+                        <input type="text" id="searchProduct" className="dropdown-item border" onChange={e => handleProductList(e.target.value)} autocomplete="off"/>
+
+                    </div>
                     {
-                        product.length > 0 ?
+                        searchProduct.length > 0 ?
                         (
-                            product.map(item => {
+                            searchProduct.map(item => {
                                 return <button key={item._id} className="dropdown-item" href="#" onClick={() => handleCart(item)}>{item.name}</button>
                             })
                         ) :
